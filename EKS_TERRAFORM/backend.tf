5da7903c001fc522d1167bc8ac920472b7c3f0ae
krishnareddygurrala <159407456+krishnareddygurrala@users.noreply.github.com>
2024-12-09 16:08:11 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -2,6 +2,4 @@ terraform {
   backend "s3" {
     bucket = "mys3bucket-achi-123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+    region = "ap-south-1"
